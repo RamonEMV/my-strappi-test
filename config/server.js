f04461c891d1ef2a518b2aea5f949dc33e7b472a
Ramón Emmanuel Melchor Villegas <ramon_melchorzx@hotmail.com>
2021-04-12 12:28:24 -0500

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1269f2ab9d2f4e539986fd505b88b724'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '68bf4961-ba29-4438-9ad3-11e983534ec5'
+};
